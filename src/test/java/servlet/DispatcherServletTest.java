b0000219de11635267b2d8694b7136821e7a51f8
leesumin <lilychouchou31@gmail.com>
2022-12-09 23:24:31 +0900

    Test: change the way ApplicationContext manages Bean

--- src/test/java/servlet/DispatcherServletTest.java
+++ src/test/java/servlet/DispatcherServletTest.java
@@ -1,5 +1,6 @@
 package servlet;
 
+import controller.WeatherController;
 import servlet.handler.HandlerMapping;
 import http.HttpRequest;
 import http.HttpResponse;
@@ -15,22 +16,25 @@ import java.lang.reflect.InvocationTargetException;
 import static org.junit.Assert.*;
 
 public class DispatcherServletTest {
-    private Servlet ds;
-    private HandlerMapping hm;
+    ApplicationContext ac;
+    Servlet ds;
+    HandlerMapping hm;
+    String httpMethod = "GET /";
+    String httpMsg = " HTTP/1.0 \r\nHost: localhost:8080 \r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 \r\n";
 
     @Before
-    public void setUp() {
-        ApplicationContext ac = ApplicationContext.getApplicationContext();
+    public void setUp() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        ac = ApplicationContext.getApplicationContext();
         ds = DispatcherServlet.getDispatcherServlet(ac);
         hm = ac.getHandlerMapping();
+        hm.addHandlerMapping("/weather", "controller.WeatherController");
+        ac.registerStorage("controller.WeatherController", new WeatherController());
     }
 
     @Test
-    public void dispatch_weatherController_test() throws IOException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        hm.addHandlerMapping("/weather", "controller.WeatherController");
+    public void dispatch_weatherController_test() throws IOException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, ClassNotFoundException {
         String path = "weather";
-        String httpMsg = "GET /"+ path + " HTTP/1.0 \r\nHost: localhost:8080 \r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 \r\n";
-        HttpRequest req = HttpUtil.parseRequest(httpMsg);
+        HttpRequest req = HttpUtil.parseRequest(httpMethod + path + httpMsg);
         HttpResponse res = new HttpResponse();
 
         ds.service(req, res);
